97324163b9289b272c3fc460dbbbd12c783d03f0
IlnurHA <Ilnur_ha@mail.ru>
2023-06-29 18:36:24 +0300

    Events updated parser

--- tg_bot/domain/domain.py
+++ tg_bot/domain/domain.py
@@ -3,36 +3,35 @@ from dataclasses import dataclass
 from typing import Optional
 
 
-"""
-class Task:
-    def __init__(self, task_name, duration, importance, start_time=None, date=None):
-        self.task_name: str = task_name
-        self.duration: int = duration
-        self.importance: int = importance
-        self.start_time: datetime.time = start_time
-        self.date: datetime.date = date
-"""
-
-
 @dataclass
 class Task:
+    telegam_id: int
     task_name: str
     duration: int
     importance: int
     complexity: int
     start_time: Optional[datetime.time] = None
-    date: Optional[datetime.date] = None
+    date: datetime.date = datetime.date.today()
+    is_done: bool = False
+    real_start: Optional[datetime.time] = None
+    real_duration: Optional[int] = None
+    real_date: Optional[datetime.date] = None
+    task_id: Optional[int] = None
 
 
 @dataclass
 class BasicUserInfo:
+    telegram_id: int
+    user_name: str
     start_time: datetime.time
     end_time: datetime.time
 
 
 @dataclass
 class Event:
+    event_id: Optional[int]
+    repeat_number: int = 0
     event_name: str
     start_time: datetime.time
     duration: int
-    repeat_arguments: Optional[str]
+    date: datetime.date = datetime.date.today()
