2ebbf57d454b2f58bab92387716adfb8e6f5a9c1
Leon-Parepko <l.parepko@innopolis.university>
2023-07-02 02:47:48 +0300

    Add data loading and model train (without backward) for model_test.

--- Data/Preprocessor.py
+++ Data/Preprocessor.py
@@ -1,13 +1,39 @@
 import pandas as pd
-from datetime import datetime
+import numpy as np
+import datetime
 from sklearn.preprocessing import MinMaxScaler
 from feature_engine.creation import CyclicalFeatures
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Task:
+    task_name: str
+    duration: int
+    importance: int
+    complexity: int
+    start_time: Optional[datetime.time] = None
+    date: Optional[datetime.date] = None
+
+
+@dataclass
+class BasicUserInfo:
+    start_time: datetime.time
+    end_time: datetime.time
+
+
+@dataclass
+class Event:
+    event_name: str
+    start_time: datetime.time
+    duration: int
+    repeat_arguments: Optional[str] = None
 
 
 class Preprocessor:
     def __init__(self):
         pass
-
 
     # there should be NLP))
     def _assign_label_number(self, label):
@@ -22,88 +48,160 @@ class Preprocessor:
         }
         return labels[label]
 
+    def _get_nearest_day(self, day_name):
+        # Get today's date
+        today = datetime.date.today()
+
+        # Map day names to weekday numbers
+        weekdays = {
+            'monday': 0,
+            'tuesday': 1,
+            'wednesday': 2,
+            'thursday': 3,
+            'friday': 4,
+            'saturday': 5,
+            'sunday': 6
+        }
+
+        # Get the weekday number of the given day_name
+        target_weekday = weekdays[day_name.lower()]
+
+        # Calculate the difference between today's weekday and the target weekday
+        days_ahead = (target_weekday - today.weekday()) % 7
+
+        # Calculate the date of the nearest day
+        nearest_day = today + datetime.timedelta(days=days_ahead)
+
+        return nearest_day
 
 
-    # change queue of tasks to dataframe
-    # it is assumed that input format is like "Sport-90-3-1-9:00-1/1/1970", two last arguments are optional
-    def _tasks_to_df(self, tasks):
-        df = pd.DataFrame(columns=['Label Number', 'Duration', 'Importance', 'Time_Min',
-                                   'Date_Day', 'Date_Month'])
+    def _parse_queue(self, queue):
+        input = pd.DataFrame(columns=['Label Number', 'Duration', 'Importance', 'Time_Min',
+                                      'Date_Day', 'Date_Month', 'Type'])
 
-        for task in tasks:
-            task = task.split('-')
+        for i in range(len(queue)):
+            if isinstance(queue[i], Task):
+                task = queue[i]
+                label_num = self._assign_label_number(task.task_name)
+                duration = task.duration
+                importance = task.importance
 
-            label_num = self._assign_label_number(task[0])
-            duration = task[1]
-            importance = task[2]
-
-            if len(task) == 6:
-                time = datetime.strptime(task[4], '%H:%M')
-                date = datetime.strptime(task[5], '%d/%m/%Y')
-                minutes = time.minute + time.hour * 60
-                day = date.day
-                month = date.month
-            elif len(task) == 5:
-                if task[4].find('/') != -1:
-                    date = datetime.strptime(task[4], '%d/%m/%Y')
+                if task.start_time is not None:
+                    time = task.start_time
+                    minutes = time.minute + time.hour * 60
+                else:
                     minutes = 0
+
+                if task.date is not None:
+                    date = task.date
                     day = date.day
                     month = date.month
                 else:
-                    time = datetime.strptime(task[4], '%H:%M')
-                    minutes = time.minute + time.hour * 60
                     day = 0
                     month = 0
+
+                input.loc[len(input)] = {'Label Number': label_num,
+                                         'Duration': duration,
+                                         'Importance': importance,
+                                         'Time_Min': minutes,
+                                         'Date_Day': day, 'Date_Month': month,
+                                         'Type': 'resched'
+                                         }
             else:
-                minutes = 0
-                day = 0
-                month = 0
+                event = queue[i]
+                label_num = self._assign_label_number(event.event_name)
+                duration = event.duration
+                importance = 3
 
-            df.loc[len(df)] = {'Label Number': label_num,
-                               'Duration': duration,
-                               'Importance': importance,
-                               'Time_Min': minutes,
-                               'Date_Day': day, 'Date_Month': month
-                               }
+                if event.start_time is not None:
+                    time = event.start_time
+                    minutes = time.minute + time.hour * 60
+                else:
+                    minutes = 0
 
-        df.sort_values(by=['Importance'], inplace=True, ascending=False)
-        df.reset_index(drop=True, inplace=True)
+                if event.repeat_arguments is not None:
+                    repeat_arguments = event.repeat_arguments.split()
+                    shift = int(repeat_arguments[0])
+                    day_name = repeat_arguments[1]
+                    repeat = int(repeat_arguments[2])
+                    for i in range(int(repeat_arguments[2])):
+                        date = self._get_nearest_day(day_name) + datetime.timedelta(days=shift * i * 7)
+                        print(date)
+                        day = date.day
+                        month = date.month
+                        input.loc[len(input)] = {'Label Number': label_num,
+                                                 'Duration': duration,
+                                                 'Importance': importance,
+                                                 'Time_Min': minutes,
+                                                 'Date_Day': day, 'Date_Month': month,
+                                                 'Type': 'non-resched'
+                                                 }
+                else:
+                    date = datetime.date.today()
+                    day = date.day
+                    month = date.month
+                    input.loc[len(input)] = {'Label Number': label_num,
+                                             'Duration': duration,
+                                             'Importance': importance,
+                                             'Time_Min': minutes,
+                                             'Date_Day': day, 'Date_Month': month,
+                                             'Type': 'non-resched'
+                                             }
 
-        return df
+        input.sort_values(by=['Importance'], inplace=True, ascending=False)
+        input.reset_index(drop=True, inplace=True)
 
+        type = input['Type'].to_numpy()
+        input.drop(columns=['Type'], inplace=True)
 
-
-    # if input format "AML-11:00-90-2 Sunday 3"  or  "AML-11:00-90-2 31 3"  or  "AML-11:00-90-1/1/1970"
-    def _events_to_arr_of_dict(self, events):
-        arr_of_dict = []
-        return arr_of_dict
-
-
-
-    def preprocess(self, data):
-
-        if not isinstance(data, pd.DataFrame):
-            # temporary solution for tasks/events distinction
-            if len(data[0].split("-")) > 4:
-                data = self._tasks_to_df(data)
+        output = np.empty(len(input), dtype=object)
+        for i in range(len(input)):
+            if type[i] == 'non-resched':
+                output[i] = np.array([input['Time_Min'][i], input['Duration'][i], 0])
             else:
-                data = self._events_to_arr_of_dict(data)
-                # special case to skip the neural network
-                pass
+                output[i] = np.nan
+
+        return input, type, output
+
+
+    def preprocess(self, queue):
+
+        if not isinstance(queue, pd.DataFrame):
+            input_vector, type_vector, output_vector = self._parse_queue(queue)
         else:
-            data["Start Time"] = pd.to_datetime(data["Start Time"], format="%H:%M")
-            data["Time_Min"] = data["Start Time"].dt.minute + data["Start Time"].dt.hour * 60
+            input_vector = queue
+            input_vector["Start Time"] = pd.to_datetime(input_vector["Start Time"], format="%H:%M")
+            input_vector["Time_Min"] = input_vector["Start Time"].dt.minute + input_vector["Start Time"].dt.hour * 60
+            input_vector["Date"] = pd.to_datetime(input_vector["Date"], format="%d/%m/%Y")
+            input_vector["Date_Day"] = input_vector["Date"].dt.day
+            input_vector["Date_Month"] = input_vector["Date"].dt.month
+            input_vector.drop(columns=["Start Time", "Date"], inplace=True)
 
-            data["Date"] = pd.to_datetime(data["Date"], format="%d/%m/%Y")
-            data["Date_Day"] = data["Date"].dt.day
-            data["Date_Month"] = data["Date"].dt.month
-
-            data.drop(columns=["Start Time", "Date"], inplace=True)
+            type_vector = np.full(len(input_vector), "resched")
+            output_vector = np.full(len(input_vector), np.nan)
 
         scaler = MinMaxScaler()
-        data['Duration'] = scaler.fit_transform(data[['Duration']])
+        input_vector['Duration'] = scaler.fit_transform(input_vector[['Duration']])
 
         cyclical = CyclicalFeatures(variables=['Time_Min', 'Date_Day', 'Date_Month'], drop_original=True, )
-        data = cyclical.fit_transform(data)
+        input_vector = cyclical.fit_transform(input_vector)
 
-        return data
+        return input_vector, type_vector, output_vector
+
+
+if __name__ == '__main__':
+    queue = [Task(task_name="Sport", duration=30, importance=3, start_time=datetime.time(hour=8, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=1),
+             Task(task_name="Food", duration=60, importance=2, start_time=datetime.time(hour=12, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=1),
+             Task(task_name="Hobby Active", duration=120, importance=1, start_time=datetime.time(hour=16, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=2),
+             Task(task_name="Hobby Passive", duration=60, importance=1, start_time=datetime.time(hour=18, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=0),
+             Task(task_name="Studying", duration=120, importance=3, start_time=datetime.time(hour=20, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=3),
+             Task(task_name="Work", duration=120, importance=3, start_time=datetime.time(hour=22, minute=0), date=datetime.date(year=2021, month=5, day=1), complexity=3),
+             Task(task_name="Other", duration=120, importance=3, start_time=datetime.time(hour=0, minute=0), date=datetime.date(year=2021, month=5, day=2), complexity=0),
+             Event(event_name="Sport", duration=120, repeat_arguments="1 monday 6", start_time=datetime.time(hour=0, minute=0)),
+             Event(event_name="Sport", duration=120, start_time=datetime.time(hour=0, minute=0))]
+
+    preprocessor = Preprocessor()
+    input_vector, type_vector, output_vector = preprocessor.preprocess(queue)
+    print(input_vector)
+    print(type_vector)
+    print(output_vector)
