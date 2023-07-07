192423089b3dbf82a8300490033d83ef7d89c383
Sofi Zaitseva <95312480+sofi12321@users.noreply.github.com>
2023-07-07 12:10:56 +0300

    Update tg bot comments

--- tg_bot/lexicon/lexicon.py
+++ tg_bot/lexicon/lexicon.py
@@ -3,66 +3,82 @@ import logging
 
 lexicon = {
     "en": {
-        "first_hello": """Hello, I am scheduler bot that will help
-        you gain healthy based productivity.
-        Please enter your name or nickname.""",
+        'user_not_found': "Hello, I don't know you. Please, write /start to register.",
+
+        "first_hello": """Hello, I'm a bot that will help
+                        you create a daily schedule. My name is InnoPlanner, and
+                        how should I call you?""",
+        "retry_name": "Can you, please, shorten your name a bit (at max 255 symbols)?",
+
+        "basic_info_start_of_day": """Now we now the names of each other! Let me say few words about me. 
+                        You probably know that it can be hard to set a realistic time for a event during scheduling. 
+                        I will analyze your event history and make your daily schedule fit your habits. 
+                        Of course, I can't force you to complete tasks as I suggested, but I hope that my advice 
+                        will help your productivity and life balance.\n
+                        My main job is to determine the order of tasks, start time, and duration. 
+                        However, all of this will be regulated using breaks between tasks.\n\n
+                        
+                        Not to distract you, please enter the time at which you usually do tasks.\n
+                        Now write the time when you start your day in a format:\n
+                        '13:00' or '1:00AM'""",
+
+        "basic_info_end_of_day": """And at which time do you end your day?""",
+        "end_first_hello": """Now we can start planning! Let me explain how to work with me. \n
+                        Here are 2 concepts: events and tasks. Events are ones that have a precise start time and 
+                        duration, I will never change these parameters. \n
+                        Tasks are items you want to complete during the day. Here you can specify the importance, 
+                        the assumed start time and duration. Based on your history, I will modify the event parameters 
+                        so that you get it done and feel good! """,
+        # "formats": """Sometimes I may not understand what you are writing. Don't worry, we have a way to communicate! When you answer my questions, please use these formats:
+        # \nDate
+        # \nTime""",
 
         "hello": "Hello, how can I help you?",
         "write_success": "Successfully saved!",
-        "add_task": """To add task, please, enter task name.""",
-        "get_duration_task": """Cool, now enter duration of task, please""",
-        "get_complexity_task": "Please, enter complexity of task",
-        "get_importance_task": "Please, enter importance of task",
-        "get_start_time_task": """Now, write time is the following format:\n\t
-        13:00 or 1:00PM\n
-        Or enter 'no' if no specific start time""",
-        "get_date_task": """Please, enter date in the following format:\n\t
-        dd/mm/yyyy\n
-        For example, 1/1/1970\n
-        Or enter 'no' if no specific date""",
-        "add_event": """To add event, please, enter event name""",
-        "event_start_time": "Now, please, enter start time of event",
-        "event_duration": f"Please, enter duration of the event up to {24 * 60}",
-        "event_date": "Please, enter date of event. If no specific date, enter 'no'",
-        "event_repeat_each_number": "Please, enter each number",
-        "event_repeat_each_argument": "Please, enter each argument",
-        "event_repeat_number_of_repetitions": "Please, enter number of repetitions",
-        "mark_history": "Choose active task. If no task, make task using '/add_task'",
-        "ask_history": """Please, enter history in the following format:\n\t
-        [start_time]-[end_time]-[is_done (true or false)]""",
+
+        "add_task": """I'm happy that you want to add a task! What is the name of the task?""",
+        "get_date_task": "When do you want to schedule it? I'm about date like 1/1/1970. If you don't care, write 'no'",
+        "get_start_time_task": """Now, what time do you want to start? For example, 13:00, 1:00PM or 'no'""",
+        "get_duration_task": """How long are you going to do it (in minutes)?""",
+        "get_importance_task": "That's an interesting task. How important is it (from 0 till 3)?",
+
+        "add_event": """New event? Cool! What is its name?""",
+
+        "event_date": "When will it be? For example, 1/1/1970",
+        "event_start_time": "What time will it start?",
+        "event_duration": f"And how long is it? (in minutes)",
+
+        "event_want_repeat": "Is it a repeatable event?",
+        "event_repeat_each_number": "How often will it repeat? For every 2nd Saturday 4 times - write 2",
+        "event_repeat_each_argument": "When will it repeat? For every 2nd Saturday 4 times - write Saturday. "
+                                      "Also, it may be day, week, month",
+        "event_repeat_number_of_repetitions": "How many times will it happen? For every 2nd Saturday 4 times - write 4",
+
+        "mark_history": "I'm glad you finished the task! Choose it from the list:",
+        
+        "marking_history_is_done": """Did you perform the task? (yes/no)""",
+        "marking_history_start_time": """When did you start task completion? ('13:00' or '1:00AM')""",
+        "marking_history_duration": """How long did this process take? (in minutes)""",
+       
+        "plan": """Today is the most important day of your life. And I'm happy that you're planning it! I'll do my best to create an optimal schedule for you""",
+        
         # retries
-        "retry_int": "Please, enter integer",
-        "retry_optional_time": "Please, enter time in the specific following format:\n\t13:00 or 1:00PM\nOr enter 'no' if no specific start time",
-        "retry_optional_date": """Please, enter date in the specific following format:\n\t
-        dd/mm/yyyy\n
-        For example, 1/1/1970\n
-        Or enter 'no' if no specific date""",
-        "retry_event": """Please, enter event in the following format:\n\t
-        [event_name]-[start_time]-[duration (in minutes)]-[Number] [day | {day of week (e.g. Sunday)} | month] [Number of events]""",
-        "retry_history": """Please, enter history in the following format:\n\t
-        [start_time]-[end_time]-[is_done (true or false)]""",
-        "retry_time": """Please, enter time in the following format:\n\t
-        '11:00' or '1:00AM'""",
-        "retry_trouble": "Sorry, unexpected error. Please, try again",
-        "error": "Got unexpected error: \n\t{}",
-
-        'user_not_found': "Please, proceed through '/start' function to register",
-        "retry_yesno": "Please enter either 'yes' or 'no'",
-        "marking_history_is_done": """Now, please tell whether you finished task or not.
-        Enter either 'yes' or 'no'""",
-        "marking_history_duration": """Please enter duration of task completion in minutes""",
-        "marking_history_start_time": """Please enter time of task start in the following format:\n\t
-        '13:00' or '1:00AM'""",
-
-        "basic_info_start_of_day": """Please enter time of your day start in
-        the following format:\n\t
-        '13:00' or '1:00AM'""",
-
-        "basic_info_end_of_day": """Please enter time of your day end in
-        the following format:\n\t
-        '13:00' or '1:00AM'""",
-
-        "retry_name": "Please enter name with length between 1 and 255"
+        "retry_int": "Please, write an integer",
+        "retry_optional_time": "Small reminder, I understand time only in this formats:\n13:00 or 1:00PM. If it does not have a specific start time, you can write 'no'",
+        "retry_optional_date": """Sorry, I didn't understand you. Please, write date in the following format:\n
+        dd/mm/yyyy. For example, 1/1/1970""",
+        "retry_time": """Ups, can you, please, repeat the time in this format: '13:00' or '1:00AM'?""",
+        "retry_trouble": "Sorry, some mistake have occured. Please, try again",
+        "error": "I got unexpected error: \n{}",
+        "retry_yesno": "This is a simple question, just write 'yes' or 'no'"
+        
+        # "ask_history": """Please, enter history in the following format:\n\t
+        # [start_time]-[end_time]-[is_done (true or false)]""",
+        # "retry_event": """Please, enter event in the following format:\n\t
+        # [event_name]-[start_time]-[duration (in minutes)]-[Number] [day | {day of week (e.g. Sunday)} | month] [Number of events]""",
+        # "retry_history": """Please, enter history in the following format:\n\t
+        # [start_time]-[end_time]-[is_done (true or false)]""",
+        
     }
 }
 
